d33829c031aa52f77b5491e6bb152b0b0abcd0ce
Rafael Romano <rafael.romano@pill.com.br>
2022-05-24 08:51:32 -0300

    starting alura tracker

---
+++ alura-tracker/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">
+      <FormComp />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue';
+import FormComp from './components/FormComp.vue'
+
+
+export default defineComponent({
+    name: "App",
+    components: { SideBar, FormComp }
+});
+</script>
+
+<style>
+
+
+</style>
